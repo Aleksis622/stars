d8c13a48883481cb8cb62fd399066e4bab09b011
Aleksis622 <ipb23.a.eglitis@vtdt.edu.lv>
2024-11-26 15:17:38 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -10,8 +10,44 @@ $data = $result->fetch_assoc();// dabuj resultatu
 
 
 var_dump($data);
+echo "<br>";
+
+echo "<pre>";
+
+var_dump($_SERVER);
+
+echo "</pre>";
+
+
+if($_SERVER["REQUEST_METHOD"]=="POST"){
+
+    $username = $_POST["username"];
+
+    $password = $_POST["password"];
+
+    $sql = "INSERT INTO users 
+    (USERNAME,PASS)
+    VALUES
+    ('$username','$password')";
+
+    $connection->query($sql);
+}
 
 echo "<form>";
-echo "<form/>";
+
+echo "<form method='POST'>";
+
+echo "  <label>Lietotajvards: <input name='username' placeholder='Lietotajvards'  /></label><br>"; 
+ 
+echo "  <label>Parole:  <input type='password' name='password' placeholder='parole' /></label><br>";
+
+echo"<input  type='submit' value='Registreties!' />";
+
+echo "</form>";
+
+
+//asociativais masivs
+//$auglisAssoc=["color"=>"sarkans","type"=>"abols", "weight"=>"120"];
+//print_r($auglisAssoc);
 
 ?>
---
+++ script.js
@@ -0,0 +1,8 @@
+ let himcook = 0;
+ 
+ function handleClick(){
+    console.log("WORKING");
+   himcook = himcook + 1;
+
+   document.getElementById("poga").textContent = himcook;
+}
